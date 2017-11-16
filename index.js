5e0fb1dd6b0eb41efe7e3370259b393e54aa4ab4
mrXCray <mr.xcray@gmail.com>
2017-11-16 04:24:58 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param url
+ * @param login
+ * @param password
+ * @constructor
+ */
+let ThycoticSecretServerClient = (url, login, password)=>{
+  "use strict";
+
+};
+
+exports.TSSClient = ThycoticSecretServerClient;
